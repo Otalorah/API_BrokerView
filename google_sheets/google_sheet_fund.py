5d692fc3a81e8b8aa79abdc860f45d840f30e427
Juan Otalora <juanmaotalora15@gmail.com>
2024-04-20 23:20:21 -0500

    Router fund agregado

---
+++ google_sheets/google_sheet_fund.py
@@ -0,0 +1,23 @@
+import gspread
+
+CREDENTIALS_FILE = "api-brokerview-45ca1238bc25.json"
+DOCUMENT = "FONDO INDIVIDUAL DE INVERSIÓN"
+
+class GoogleSheet_fund:
+    def __init__(self, sheet_name:str):
+        self.gc = gspread.service_account(filename=CREDENTIALS_FILE)
+        self.sh = self.gc.open(DOCUMENT)
+        self.sheet = self.sh.worksheet(sheet_name)
+
+    def get_num_rows(self) -> str:
+        
+        last_row = len(self.sheet.col_values(1))
+
+        return f"{last_row}"
+    
+    def read_data(self) -> list[list]:
+
+        num_rows = self.get_num_rows()
+        range_values = f'A3:F{num_rows}'
+
+        return self.sheet.get(range_name=range_values)
