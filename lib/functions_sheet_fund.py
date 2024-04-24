c8b21172aea0914819f166112fb523eabd6f87d8
Juan Otalora <juanmaotalora15@gmail.com>
2024-04-24 18:57:38 -0500

    Mejora path fund

--- lib/functions_sheet_fund.py
+++ lib/functions_sheet_fund.py
@@ -4,31 +4,16 @@ from google_sheets.google_sheet_fund import GoogleSheet_fund
 def read_data_sheet(sheet_name: str) -> list[list]:
 
     sheet = GoogleSheet_fund(sheet_name=sheet_name)
-
     return sheet.read_data()
 
 
-def convert_to_dictionary(data: list[list]) -> dict:
+def convert_to_dictionary(data: list[list]) -> list[dict]:
 
-    dict_data = dict()
-
-    num_rows = 1
-    previous_row_repeated = False
+    list_data = list()
+    COLUMNS_NAME = ['FECHA CORTE', 'APORTE', 'FECHA DE APORTE',
+                    'SALDO ANTERIOR', 'RENDIMIENTOS', 'SALDO ACTUAL']
 
     for row in data:
+        list_data.append(dict(zip(COLUMNS_NAME, row)))
 
-        cutoff_date = row[0]
-        new_row = row[1:]
-
-        if cutoff_date in dict_data:
-            num_rows += 1
-            dict_data[cutoff_date][num_rows] = new_row
-            previous_row_repeated = True
-            continue
-        elif previous_row_repeated:
-            num_rows = 1
-            previous_row_repeated = False
-
-        dict_data[cutoff_date] = {num_rows: new_row}
-
-    return dict_data
+    return list_data
--- routers/fund.py
+++ routers/fund.py
@@ -10,9 +10,11 @@ router = APIRouter()
 # - - - - - - - - - - - - - - - - - - - - - - - - - ENDPOINTS - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 #get data from fund sheet
-@router.get("/", response_model=dict, status_code=status.HTTP_200_OK)
-async def get_data_fund(token: Annotated[None, Depends(aut_token)]) -> dict:
+
+
+@router.get("/", response_model=list, status_code=status.HTTP_200_OK)
+async def get_data_fund(token: Annotated[None, Depends(aut_token)]) -> list[dict]:
 
     user_sheet = token['user_sheet']
     data_list = read_data_sheet(user_sheet)
-    return convert_to_dictionary(data_list)
+    return convert_to_dictionary(data=data_list)
