63d11e009176ec0b5d0d474d597187326fec3d33
Juan Manuel Otálora Hernández <juanmaotalora15@gmail.com>
2024-04-30 08:28:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,10 @@ from dotenv import load_dotenv
 app = FastAPI()
 
 load_dotenv()
- 
-origins = ["http://localhost:4321/","http://localhost:4321"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
