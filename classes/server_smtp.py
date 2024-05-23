2d8e1ea2c7eec48242ac444b1635ac6c5c17d6b7
Juan Otalora <juanmaotalora15@gmail.com>
2024-05-23 12:20:04 -0500

    Mejoras server smtp

--- classes/server_smtp.py
+++ classes/server_smtp.py
@@ -1,40 +1,36 @@
-import smtplib
+from smtplib import SMTP_SSL
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from smtplib import SMTP_SSL
 
 from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv()
 
-SMTP_USER = getenv('SMTP_USER', 'brokerviewes@gmail.com')
-SMTP_PASSWORD = getenv('SMTP_PASSWORD', 'wvwx xfvm yzoc aoua')
+SMTP_USER = getenv('SMTP_USER')
+SMTP_PASSWORD = getenv('SMTP_PASSWORD')
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 465
 
 
 class ServerSMTP:
 
-    def send_email(self, to_email, body):
+    def send_email(self, email, body):
 
-        msg = self.build_message(to_email, body)
+        msg = self.build_message(email, body)
 
         with SMTP_SSL(SMTP_SERVER, SMTP_PORT) as server:
-            try:
-                server.ehlo()
-                server.login(SMTP_USER, SMTP_PASSWORD)
-                server.sendmail(SMTP_USER, to_email, msg.as_string())
-            except smtplib.SMTPException as e:
-                print(f"Error al enviar el correo: {e}")
+            server.ehlo()
+            server.login(SMTP_USER, SMTP_PASSWORD)
+            server.sendmail(SMTP_USER, email, msg.as_string())
 
     @staticmethod
-    def build_message(to_email, body) -> MIMEMultipart:
+    def build_message(email, body) -> MIMEMultipart:
 
         msg = MIMEMultipart()
         msg['From'] = "brokerviewes@gmail.com"
-        msg['To'] = to_email
+        msg['To'] = email
         msg['Subject'] = "Código de verificación"
         msg.attach(MIMEText(body, 'plain'))
 
